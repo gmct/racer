71115624f8324cfaff2ceffabbb0ed88b95501fc
rhandyrhoads <gcosteas@gmail.com>
2015-11-18 11:45:37 -0500

    Basic Graphic to Start

--- AccelRace/src/racer_local/Racer.java
+++ AccelRace/src/racer_local/Racer.java
@@ -1,47 +1,43 @@
-/* ToDo
- * Display the grid.
- * Create track on grid.
- * Create start and finish lines.
- * Create car elements.
- * Accept input to move cars.
- * Make input only allowed to increase by 1.
- * Ensure that location doesn't equal or pass the wall. 
- * Ensure line doesn't intersect the wall.
- */
-
 package racer_local;
 
-import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+@SuppressWarnings("serial")
+public class Racer extends JPanel {
 
-public class Racer extends JFrame {
-    
-    public Racer() {
+	int x = 0;
+	int y = 0;
 
-        initUI();
-    }
+	private void moveCar() {
+		x = x + 1;
+		y = y + 1;
+	}
 
-    private void initUI() {
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.fillOval(x, y, 30, 30);
+	}
 
-        add(new RaceTrack());
-
-        setSize(250, 200);
-
-        setTitle("Application");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-    }    
-    
-    public static void main(String[] args) {
-        
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                Racer ex = new Racer
-                		();
-                ex.setVisible(true);
-            }
-        });
-    }
+	public static void main(String[] args) throws InterruptedException {
+		JFrame raceTrack = new JFrame("Mini Tennis");
+		Racer racer = new Racer();
+		raceTrack.add(racer);
+		raceTrack.setSize(300, 400);
+		raceTrack.setVisible(true);
+		raceTrack.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		while (true) {
+			racer.moveCar();
+			racer.repaint();
+			Thread.sleep(10);
+		}
+	}
 }

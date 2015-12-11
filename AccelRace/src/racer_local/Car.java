3c48e3ffa7b2cfc91cef7363c9d269db14de5fc1
rhandyrhoads <gcosteas@gmail.com>
2015-12-11 10:12:16 -0500

    Reimplemented clean passive animation code

--- AccelRace/src/racer_local/Car.java
+++ AccelRace/src/racer_local/Car.java
@@ -4,16 +4,14 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
-import racer_local.*;
-
-public class Car{
+public class Car extends Racer{
 	public int x;
 	public int y;
 	public String color;
-	private int[] coords = new int[2];
-	private int[] lastCoords = new int[2];
-	private int xVelocity;
-	private int yVelocity;
+	public int[] coords = new int[2];
+	public int[] lastCoords = new int[2];
+	public int xVelocity;
+	public int yVelocity;
 	
 	public Car(int x, String color){
 		this.x = x;
@@ -29,12 +27,11 @@ public class Car{
 		y += yVelocity;
 		
 		// Visualizes the movement of the car.
-		//Graphics g = raceTrack.getGraphics();
-		//g.setColor(Color.red);
-		//drawVelocity(g);
-		//drawCar(g);
-		//drawGrid(g, 700, 500);
-		//g.dispose();
+		Graphics g = raceTrack.getGraphics();
+		g.setColor(Color.red);
+		// drawVelocity(g);
+		drawCar(g);
+		g.dispose();
 	}
 	
 	private void drawCar(Graphics g){
@@ -43,17 +40,16 @@ public class Car{
 		
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setPaint(Color.red);
-		g2d.fillOval(x * 10, 500 - y * 10, 10, 10);
+		g2d.fillOval(x * 6, 500 - y * 6, 6, 6);
 		// TODO: Separate this code into more easy to read lines. Maybe do this by making graphX/Y vars.
-		g2d.drawLine(xOld * 10 + 5, 500 - (yOld * 10 - 5), x * 10 + 5, 500 - (y * 10 - 5));
-		g2d.setPaint(Color.black);
+		g2d.drawLine(xOld * 6 + 3, 500 - (yOld * 6 - 3), x * 6 + 3, 500 - (y * 6 - 3));
+		g2d.dispose();
 	}
 	
 	private void drawVelocity(Graphics g){
 		// For some reason this line causes program to freeze.
 		 g.drawString(Integer.toString(xVelocity), 20, 30);
 	}
-	
 	public int[] getVelocity(){
 		int[] velocities = {xVelocity, yVelocity};
 		return velocities;
--- AccelRace/src/racer_local/Racer.java
+++ AccelRace/src/racer_local/Racer.java
@@ -11,69 +11,53 @@ package racer_local;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JPanel;
 import javax.swing.JButton;
-import racer_local.*;
+import racer_local.Car;
 
 @SuppressWarnings("serial")
-public class Racer{
+public class Racer extends JFrame{
+	static JFrame raceTrack = new JFrame("Racer");
+	
 	public static void main(String[] args){
-		RaceTrack raceTrack = new RaceTrack("Racer", 700, 500);
-		raceTrack.drawGrid(5);
-//		Scanner scan = new Scanner(System.in);
-//		Car racer = new Car(2, "red");
-//		int xChange;
-//		int yChange;
-//		
-//		while(true){
-//			System.out.print("Please enter a change in x velocity (-1, 0, or 1): ");
-//			// Temporary solution until I can implement a GUI based input.
-//			try{
-//				xChange = Integer.parseInt(scan.nextLine());
-//				if (xChange < -1 || xChange > 1){
-//					System.out.println("Invalid parameter.");
-//					continue;
-//				}
-//			} catch (NumberFormatException e){
-//				System.err.println("Invalid input.");
-//				System.out.println();
-//				continue;
-//			}
-//			
-//			System.out.print("Please enter a change in y velocity (-1, 0, or 1): ");
-//			
-//			try{
-//				yChange = Integer.parseInt(scan.nextLine());
-//				if (yChange < -1 || yChange > 1){
-//					System.out.println("Invalid parameter.");
-//					continue;
-//				}
-//			}catch(NumberFormatException e){
-//				System.err.println("Invalid input.");
-//				System.out.println();
-//				continue;
-//			}
-//			
-//			racer.moveCar(xChange, yChange);
-//		}
+		Scanner scan = new Scanner(System.in);
+		Car racer = new Car(2, "red");
+		int xChange = 0;
+		int yChange;
+		raceTrack.setSize(700, 500);
+		raceTrack.setVisible(true);
+		raceTrack.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		drawGrid(700, 500);
+		
+		while(true){
+			System.out.print("Please enter a change in x velocity (-1, 0, or 1): ");
+			xChange = scan.nextInt();
+			if (xChange < -1 || xChange > 1){
+				System.out.println("Invalid parameter.");
+				continue;
+			}
+			
+			System.out.print("Please enter a change in y velocity (-1, 0, or 1): ");
+			yChange = scan.nextInt();
+			if (yChange < -1 || yChange > 1){
+				System.out.println("Invalid parameter.");
+				continue;
+			}
+			
+			racer.moveCar(xChange, yChange);
+		}
 	}
-//	public static void drawGrid(int XSIZE, int YSIZE){
-//		// Code broken. Figure out how to draw line here and then reimplement for loops.
-//		// Separate code into RaceTrack which will handle the graphics for the raceTrack.
-//		Graphics g = raceTrack.getGraphics();
-//		Graphics2D g2d = (Graphics2D) g;
-//		g2d.setPaint(Color.black);
-//		for (int i = 5; i < XSIZE; i += 5){
-//			g2d.drawLine(i, 0, i, YSIZE);
-//		}
-//		for (int i = 5; i < YSIZE; i += 5){
-//			g2d.drawLine(0, i, XSIZE, i);
-//		}
-//		g2d.dispose();
-//	}
+	public static void drawGrid(int XSIZE, int YSIZE){
+		// Code broken. Figure out how to draw line here and then reimplement for loops.
+		Graphics g = raceTrack.getGraphics();
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setPaint(Color.black);
+		g2d.drawLine(40, 70, 100, 90);
+		g2d.dispose();
+		g.dispose();
+	}
 }
